a61bbbd1966cabdb34f0c9f9beafbd0bb37fa030
Lan <46338530+IOUKI@users.noreply.github.com>
2024-09-30 16:31:45 +0800

    fix: image src

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <>
       <div className="text-9xl text-gray-800">大家好！！！</div>
-      <Image src="/test.jpg" alt="" width={200} height={200} />
+      <Image src="./test.jpg" alt="" width={200} height={200} />
     </>
   )
 }
