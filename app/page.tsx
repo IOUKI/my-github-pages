047d11077783596ca1e3b1fb73bc9e8c92e0f5c2
Lan <46338530+IOUKI@users.noreply.github.com>
2024-09-30 16:29:06 +0800

    feat: test image

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Home = () => {
   return (
-    <div className="text-9xl text-gray-800">大家好！！！</div>
+    <>
+      <div className="text-9xl text-gray-800">大家好！！！</div>
+      <Image src="/test.jpg" alt="" width={200} height={200} />
+    </>
   )
 }
 
