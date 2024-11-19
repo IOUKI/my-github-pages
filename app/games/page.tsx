f3e87384418d00a65a6225c7adb1b80a70923695
Lan <quw112233456@gmail.com>
2024-11-19 23:26:56 +0800

    setup games home page

---
+++ app/games/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GameHomePage = () => {
+  return (
+    <div className="w-full h-screen bg-black flex justify-center items-center">
+      <h1 className="text-9xl text-white">
+        Comming soon...
+      </h1>
+    </div>
+  )
+}
+
+export default GameHomePage
