e4da27a93788fddbda79bb49e4b78db107416546
Lan <quw112233456@gmail.com>
2024-11-19 23:30:12 +0800

    refactor: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import Image from 'next/image'
-import ThemeSwitcher from '@/components/ThemeSwitcher'
 
 const Home = () => {
   return (
     <>
-      <ThemeSwitcher />
-      <div className="text-9xl">大家好！！！</div>
-      <Image src="./test.jpg" alt="" width={200} height={200} />
+      Home
     </>
   )
 }
