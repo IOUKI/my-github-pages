6cb66a9f8b3971b6c4c8ae6ba17140973fc12682
Lan <quw112233456@gmail.com>
2024-11-21 00:05:59 +0800

    feat: 自動導向profile

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+'use client'
+
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 const Home = () => {
-  return (
-    <>
-      Home
-    </>
-  )
+  const [isClient, setIsClient] = useState(false)
+  const router = useRouter()
+  useEffect(() => setIsClient(true), [])
+  useEffect(() => {
+    if (isClient) router.push('/profile')
+  }, [isClient])
+  return null
 }
 
 export default Home
