5b6ee4e64cb98453ea6d67d0ac4ea62151bf70e6
Lan <quw112233456@gmail.com>
2024-12-09 21:57:06 +0800

    refactor: header button style

--- app/games/layout.tsx
+++ app/games/layout.tsx
@@ -8,14 +8,16 @@ export default function GamesLayout({ children }: { children: React.ReactNode })
     <>
       <div className="flex flex-col mx-auto size-full">
         <main className="min-h-[100vh]">
-          <div className="fixed top-0 left-0 p-3">
-            <Link href="/profile" className="text-xl">
-              <i className="bi bi-person-circle"></i>&nbsp;
-              <span>Profile</span>
-            </Link>
-          </div>
-          <div className="fixed top-0 right-0 p-2">
-            <ThemeSwitcher size={32} />
+          <div className="w-full flex justify-between">
+            <div className="p-3">
+              <Link href="/profile" className="text-xl">
+                <i className="bi bi-person-circle"></i>&nbsp;
+                <span>Profile</span>
+              </Link>
+            </div>
+            <div className="p-3">
+              <ThemeSwitcher size={32} />
+            </div>
           </div>
           {children}
         </main>
