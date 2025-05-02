aa844be5024ba7172adb3b03ec7d266250a8a4c6
Lan <quw112233456@gmail.com>
2025-05-02 10:42:44 +0800

    refactor: kendama tool layout 左上角連結

--- app/kendamaTools/layout.tsx
+++ app/kendamaTools/layout.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import Link from "next/link"
 import PrelineScript from "@/components/PrelineScript"
 import ThemeSwitcher from "@/components/ThemeSwitcher"
+import Image from "next/image"
 
 export default function GamesLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,9 +11,9 @@ export default function GamesLayout({ children }: { children: React.ReactNode })
         <main className="min-h-[100vh]">
           <div className="w-full flex justify-between">
             <div className="p-3">
-              <Link href="/profile" className="text-xl">
-                <i className="bi bi-person-circle"></i>&nbsp;
-                <span>Profile</span>
+              <Link href="/kendamaTools" className="text-xl flex items-center">
+                <Image src="/kendama.png" alt="kendama" width={30} height={30} />
+                <span>劍玉</span>
               </Link>
             </div>
             <div className="p-3">
