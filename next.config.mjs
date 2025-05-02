9c7be230589693fd52ef96568f9cdb4cd4c25435
Lan <quw112233456@gmail.com>
2025-05-02 11:30:30 +0800

    fix: assets prefix

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,8 @@ const nextConfig = {
     distDir: 'dist',
     images: {
         unoptimized: true,
-    }
+    },
+    assetPrefix: isProd ? '/my-github-pages/' : '',
 };
 
 export default nextConfig;
